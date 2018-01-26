c9f361659c6d7c03d893cf8db0f4c971a7f8e577
obushakim <irfanhakimobuz@gmail.com>
2018-01-26 16:01:43 +0700

    Initial commit

---
+++ src/main/java/com/example/websoocketdemo/WesocketDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.websoocketdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WesocketDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WesocketDemoApplication.class, args);
+	}
+}
